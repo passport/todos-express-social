4c255790244e5e707882a3026abf722dc8fb74ed
Jared Hanson <jaredhanson@gmail.com>
2021-06-22 22:06:34 -0700

    Remove strategy config from boot/auth.

--- boot/auth.js
+++ boot/auth.js
@@ -1,31 +1,8 @@
 var passport = require('passport');
-var Strategy = require('passport-google-oauth20');
 
 
 module.exports = function() {
   
-  // Configure the Facebook strategy for use by Passport.
-  //
-  // OAuth 2.0-based strategies require a `verify` function which receives the
-  // credential (`accessToken`) for accessing the Facebook API on the user's
-  // behalf, along with the user's profile.  The function must invoke `cb`
-  // with a user object, which will be set at `req.user` in route handlers after
-  // authentication.
-  passport.use(new Strategy({
-      clientID: process.env['GOOGLE_CLIENT_ID'],
-      clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-      callbackURL: '/oauth2/redirect/accounts.google.com',
-      scope: [ 'profile' ]
-    },
-    function(accessToken, refreshToken, profile, cb) {
-      // In this example, the user's Facebook profile is supplied as the user
-      // record.  In a production-quality application, the Facebook profile should
-      // be associated with a user record in the application's database, which
-      // allows for account linking and authentication with other identity
-      // providers.
-      return cb(null, profile);
-    }));
-    
   // Configure Passport authenticated session persistence.
   //
   // In order to restore authentication state across HTTP requests, Passport needs
