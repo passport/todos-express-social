5a9a69bc94764a49e60d28fb4246ac16caf0e29d
Jared Hanson <jaredhanson@gmail.com>
2021-06-22 22:11:22 -0700

    Factor out single sign on middleware.

--- routes/auth.js
+++ routes/auth.js
@@ -31,6 +31,54 @@ function createStrategy(provider) {
   
 }
 
+function singleSignOn(req, res, next) {
+  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+    'https://' + req.params.provider,
+    req.federatedUser.id
+  ], function(err, row) {
+    if (err) { return next(err); }
+    if (!row) {
+      db.run('INSERT INTO users (name) VALUES (?)', [
+        req.federatedUser.displayName
+      ], function(err) {
+        if (err) { return next(err); }
+        
+        var id = this.lastID;
+        db.run('INSERT INTO federated_credentials (provider, subject, user_id) VALUES (?, ?, ?)', [
+          'https://' + req.params.provider,
+          req.federatedUser.id,
+          id
+        ], function(err) {
+          if (err) { return next(err); }
+          var user = {
+            id: id.toString(),
+            displayName: req.federatedUser.displayName
+          };
+          req.login(user, function(err) {
+            if (err) { return next(err); }
+            res.redirect('/');
+          });
+        });
+      });
+    } else {
+      db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
+        if (err) { return next(err); }
+  
+        // TODO: Handle undefined row.
+        var user = {
+          id: row.id.toString(),
+          username: row.username,
+          displayName: row.name
+        };
+        req.login(user, function(err) {
+          if (err) { return next(err); }
+          res.redirect('/');
+        });
+      });
+    }
+  });
+}
+
 
 var router = express.Router();
 
@@ -49,54 +97,7 @@ router.get('/oauth2/redirect/:provider',
     var strategy = createStrategy(req.params.provider);
     passport.authenticate(strategy, { assignProperty: 'federatedUser', failureRedirect: '/login' })(req, res, next);
   },
-  function(req, res, next) {
-    db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-      'https://' + req.params.provider,
-      req.federatedUser.id
-    ], function(err, row) {
-      if (err) { return next(err); }
-      if (!row) {
-        db.run('INSERT INTO users (name) VALUES (?)', [
-          req.federatedUser.displayName
-        ], function(err) {
-          if (err) { return next(err); }
-          
-          var id = this.lastID;
-          db.run('INSERT INTO federated_credentials (provider, subject, user_id) VALUES (?, ?, ?)', [
-            'https://' + req.params.provider,
-            req.federatedUser.id,
-            id
-          ], function(err) {
-            if (err) { return next(err); }
-            var user = {
-              id: id.toString(),
-              displayName: req.federatedUser.displayName
-            };
-            req.login(user, function(err) {
-              if (err) { return next(err); }
-              res.redirect('/');
-            });
-          });
-        });
-      } else {
-        db.get('SELECT rowid AS id, username, name FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
-          if (err) { return next(err); }
-    
-          // TODO: Handle undefined row.
-          var user = {
-            id: row.id.toString(),
-            username: row.username,
-            displayName: row.name
-          };
-          req.login(user, function(err) {
-            if (err) { return next(err); }
-            res.redirect('/');
-          });
-        });
-      }
-    });
-    
-  });
+  singleSignOn);
 
 router.get('/logout', function(req, res, next) {
   req.logout();
