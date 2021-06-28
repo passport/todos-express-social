167db4325d485001d0de6661452e31c9a2a4522b
Jared Hanson <jaredhanson@gmail.com>
2021-06-28 10:42:16 -0700

    Load connected accounts.

--- routes/myaccount.js
+++ routes/myaccount.js
@@ -3,6 +3,7 @@ var csrf = require('csurf');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 var passport = require('passport');
 var idp = require('../lib/idp');
+var db = require('../db');
 
 var router = express.Router();
 
@@ -17,6 +18,28 @@ router.get('/', ensureLoggedIn(), function(req, res, next) {
 router.get('/connected',
   csrf(),
   ensureLoggedIn(),
+  function(req, res, next) {
+    db.all('SELECT * FROM federated_credentials WHERE user_id = ?', [ req.user.id ], function(err, rows) {
+      if (err) { return next(err); }
+      
+      rows.forEach(function(row) {
+        var acct = {
+          provider: row.provider,
+          subject: row.subject
+        };
+        
+        switch (row.provider) {
+        case 'https://www.facebook.com':
+          res.locals.facebookAccount = acct;
+          break;
+        case 'https://accounts.google.com':
+          res.locals.googleAccount = acct;
+          break;
+        }
+      });
+      next();
+    });
+  },
   function(req, res, next) {
     res.render('myaccount/connected', { user: req.user, csrfToken: req.csrfToken() });
   });
