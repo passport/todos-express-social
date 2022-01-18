6f43ff84ec19b46216c46f070f4f7679139a992a
Jared Hanson <jaredhanson@gmail.com>
2022-01-18 15:28:28 -0800

    Update views.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
 var express = require('express');
+var db = require('../db');
+
+function fetchTodos(req, res, next) {
+  db.all('SELECT rowid AS id, * FROM todos WHERE owner_id = ?', [
+    req.user.id
+  ], function(err, rows) {
+    if (err) { return next(err); }
+    
+    var todos = rows.map(function(row) {
+      return {
+        id: row.id,
+        title: row.title,
+        completed: row.completed == 1 ? true : false,
+        url: '/' + row.id
+      }
+    });
+    res.locals.todos = todos;
+    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
+    res.locals.completedCount = todos.length - res.locals.activeCount;
+    next();
+  });
+}
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (!req.user) { return res.render('home'); }
+  next();
+}, fetchTodos, function(req, res, next) {
+  res.locals.filter = null;
   res.render('index', { user: req.user });
 });
 
